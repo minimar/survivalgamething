8e50f1afe8e3bf0b1db07f1e3f1e814438c1ce69
minimar <minimarGOO@gmail.com>
2024-03-31 03:51:47 +0100

    Interaction System

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 
 const MovementSpeed = 100
+const dialogueOffset = .18
 
-
+@onready var dialogueArea = $Area2D
+signal dialogueSignal
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -14,6 +16,21 @@ func _physics_process(delta):
 	velocity = Vector2(Input.get_axis("ui_left","ui_right"), Input.get_axis("ui_up","ui_down"))
 	velocity = velocity.normalized()*MovementSpeed
 	move_and_slide()
+	if velocity != Vector2(0,0):
+		dialogueArea.position = Vector2(0,12)
+		dialogueArea.position += velocity * dialogueOffset
 	#print(position)
 	
+func _process(delta):
+	if Input.is_action_just_pressed("Interact"):
+		checkDialogueNodes()
 
+func checkDialogueNodes():
+	var dialogueNodes = dialogueArea.get_overlapping_areas()
+	if dialogueNodes.size() == 0:
+		return
+	var priorityDialogueNode = dialogueNodes[0]
+	for dialogueNode in dialogueNodes:
+		if dialogueNode.dialoguePriority > priorityDialogueNode.dialoguePriority:
+			priorityDialogueNode = dialogueNode
+	dialogueSignal.emit(priorityDialogueNode)
---
+++ Scripts/dialogue_area.gd
@@ -0,0 +1,6 @@
+extends Area2D
+class_name dialogueNode
+@export var dialogueID = ""
+@export var dialogueText = ""
+@export var dialoguePriority = 1
+
