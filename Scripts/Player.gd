f3901ce7aa701622cf22459737a49aae7b729400
Chromaphasia <baron11704@icloud.com>
2024-03-30 21:40:29 -0400

    Cabin start

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -14,8 +14,6 @@ func _physics_process(delta):
 	velocity = Vector2(Input.get_axis("ui_left","ui_right"), Input.get_axis("ui_up","ui_down"))
 	velocity = velocity.normalized()*MovementSpeed
 	move_and_slide()
-	position.x = round(position.x)
-	position.y = round(position.y)
 	#print(position)
 	
 
